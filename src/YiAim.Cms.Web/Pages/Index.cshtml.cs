2dfa687977eaca81119ae56862a9bd51fa5ff1d6
t-jian <13768073860@163.com>
2022-11-24 09:19:44 +0800

    q

--- src/YiAim.Cms.Web/Pages/Index.cshtml.cs
+++ src/YiAim.Cms.Web/Pages/Index.cshtml.cs
@@ -1,9 +1,20 @@
-﻿namespace YiAim.Cms.Web.Pages;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using YiAim.Cms.Blogs;
+
+namespace YiAim.Cms.Web.Pages;
 
 public class IndexModel : CmsPageModel
 {
-    public void OnGet()
+    private readonly IBlogService _BlogService;
+    public IndexModel(IBlogService BlogService)
     {
-
+        _BlogService = BlogService;
+    }
+    public PagedResultDto<PageBlogDto> Blogs { get; set; }
+    public async Task OnGet()
+    {
+        Blogs = await _BlogService.GetListAsync(new PagedAndSortedResultRequestDto() { SkipCount = 0, MaxResultCount = 10 });
     }
 }
